f8bc770330317403eef787aab3a5e19c8a2585c4
LatinSuD <latinsud@gmail.com>
2020-04-12 20:38:18 +0200

    Arreglarlo

--- betterfc.user.js
+++ betterfc.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Forocoches
 // @namespace    https://latinsud.com/
-// @version      0.1
+// @version      0.2
 // @description  Pequenas mejoras a FC
 // @author       LatinSuD
 // @match        https://www.forocoches.com/*
@@ -36,90 +36,89 @@ function functionEval(source) {
 
 (function() {
 
-   console.log("FC Better 2")
+
+    // INDICE
+    if (location.href=='https://www.forocoches.com/' || location.href=='https://www.forocoches.com/#') {
+        console.log("FC Better 2")
+
+        // cambiar color
+        $('TD[BGCOLOR="#fbfbfd"]').attr('bgcolor','#eee');
+
+
+        // expandir titulos acortados por los puntos suspensivos
+        var regex = new RegExp('\\.\\.\\.$');
+        $('.cajasnews A.texto').filter(function () {
+            return regex.test($(this).text());
+        }).each(function( index, value ) {
+            if (value.title) value.textContent = value.title;
+        });
+
+        // encontrar posts con 0 respuestas
+        $('.cajasnews TD.texto').
+        filter(function () {
+            var regex2 = new RegExp('^0?$');
+            var resul= (
+                ( regex2.test($(this).text()) )
+                &&
+                (
+                    $(this).closest('TR').find("A").filter(function () {
+                        return $(this).text()=='General';
+                    }).length>0
+                )
+            );
+            if (resul)
+                console.log($(this));
+            return resul;
+        }).
+        closest('TR').
+        find("A").
+        each(function( index, value ) {
+            if (value) value.style.color='#7a0';
+        });
+
+        // Detectar ciertos nicks
+        //  Si es mas de 10 se corta a 8 y se añaden 2 puntos
+        // TODO: Esto debería ser editable de manera amigable
+        regex = new RegExp('^(nickamigo1|nickamigo2)$');
+
+        $('.cajasnews A').
+        filter(function () {
+            return regex.test($(this).text());
+        }).
+        closest('TR').
+        find("A").
+        each(function( index, value ) {
+
+            if (value) value.style.color=GM_getValue('color_amigo','#f44');
+        });
+    }
 
 
 
+    /*
+    // QUITAR PUBLI.
+    {
+        var targetNode = document.body;
 
-// INDICE
-if (location.href=='https://www.forocoches.com/' || location.href=='https://www.forocoches.com/#') {
-	// cambiar color
-	$('TD[BGCOLOR="#fbfbfd"]').attr('bgcolor','#eee');
+        // Options for the observer (which mutations to observe)
+        var config = { attributes: true, childList: true, subtree: true };
 
+        // Callback function to execute when mutations are observed
+        var callback = function(mutationsList) {
 
-	// expandir titulos acortados por los puntos suspensivos
-	var regex = new RegExp('\\.\\.\\.$');
-	$('.cajasnews A.texto').filter(function () {
-	    return regex.test($(this).text());
-	}).each(function( index, value ) {
-	  if (value.title) value.textContent = value.title;
-	});
+            $('.tg-adhesion-bar').css('display','none');
+            $('#tg-adhesion').css('display','none');
+            $('#opd_bottomstickyad').css('display','none');
 
-	// encontrar posts con 0 respuestas
-	$('.cajasnews TD.texto').
-	  filter(function () {
-	    var regex2 = new RegExp('^0?$');
-	    var resul= (
-		( regex2.test($(this).text()) )
-		&&
-		(
-		 $(this).closest('TR').find("A").filter(function () {
-			return $(this).text()=='General';
-		 }).length>0
-		)
-	    );
-	    if (resul)
-	      console.log($(this));
-	    return resul;
-	  }).
-	  closest('TR').
-          find("A").
-	  each(function( index, value ) {
-	     if (value) value.style.color='#7a0';
-	});
+        };
 
-	// Detectar ciertos nicks
-	//  Si es mas de 10 se corta a 8 y se añaden 2 puntos
-  // TODO: Esto debería ser editable de manera amigable
-	regex = new RegExp('^(nickamigo1|nickamigo2)$');
+        // Create an observer instance linked to the callback function
+        var observer = new MutationObserver(callback);
 
-	$('.cajasnews A').
-	  filter(function () {
-	    return regex.test($(this).text());
-	  }).
-	  closest('TR').
-          find("A").
-	  each(function( index, value ) {
+        // Start observing the target node for configured mutations
+        observer.observe(targetNode, config);
 
-	     if (value) value.style.color=GM_getValue('color_amigo','#f44');
-	  });
-}
+    }
+    */
 
-
-
-/*
-// QUITAR PUBLI.
-{
-	var targetNode = document.body;
-
-	// Options for the observer (which mutations to observe)
-	var config = { attributes: true, childList: true, subtree: true };
-
-	// Callback function to execute when mutations are observed
-	var callback = function(mutationsList) {
-
-  $('.tg-adhesion-bar').css('display','none');
-  $('#tg-adhesion').css('display','none');
-  $('#opd_bottomstickyad').css('display','none');
-
-	};
-
-	// Create an observer instance linked to the callback function
-	var observer = new MutationObserver(callback);
-
-	// Start observing the target node for configured mutations
-	observer.observe(targetNode, config);
-  */
-}
-
-  
+})();
